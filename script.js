a79bbb52e0f2eef613f1049592ede56184f02a5c
Shivam Ranjan <shivamranjan68@gmail.com>
2024-01-01 14:32:55 +0530

    Now save the task list in local storage

--- script.js
+++ script.js
@@ -1,78 +1,87 @@
 let ul = document.querySelector('ul');
 let input = document.querySelector('#task');
 let addBtn = document.querySelector('#add');
-let lists = [];
-let para = [];
-let tick = [];
-let del = [];
-let taskDone = [];
+
 let i = 0;
 let list = document.querySelectorAll('ul li');
+get();
 addBtn.addEventListener('click', () => {
-
     if (input.value == "") {
-        alert("Please Enter a task first");
+        alert("Please Enter a Task First");
     }
     else {
         let li = document.createElement('li');
         let p = document.createElement('p');
-        let span = document.createElement('span');
-        span.setAttribute('class', "material-symbols-outlined");
-        span.innerText = "close";
-        del[i] = span;
         p.innerText = input.value;
-        para[i] = p;
-        let img = document.createElement('img');
-        tick[i] = img;
-        img.setAttribute('src', "images/unchecked.png");
-        li.insertAdjacentElement("afterbegin", img);
         li.appendChild(p);
-        li.insertAdjacentElement("beforeend", span);
-
+        let span = document.createElement('span');
+        span.setAttribute('class', 'material-symbols-outlined');
+        span.innerText = "close";
+        li.appendChild(span);
+        li.setAttribute('class', 'uncheck');
         ul.appendChild(li);
         list = document.querySelectorAll('ul li');
-        lists[i] = li;
-        taskDone[i] = false;
-        i++;
-        input.value = "";
-        for (lis of lists) {
-            
-            if(lis.onclick==null){
-            lis.onclick = function (event) {
-                let n = lists.indexOf(this);
-            if(taskDone[n]==true){
-                if(event.target!=span){
-                
-                    para[n].innerHTML = para[n].innerText ;
-                    tick[n].setAttribute('src', "images/unchecked.png");
-                    taskDone[n] = false;
-                    }
-            }    
-            else{
-                if(event.target!=span){
-                
-                    para[n].innerHTML = "<s>" + para[n].innerText + "</s";
-                    tick[n].setAttribute('src', "images/checked.png");
-                    taskDone[n] = true;
-                    }
+        input.value = '';
+       
+        li.onclick= function (event) {
+
+            if (event.target == span) {
+                ul.removeChild(this);
+                save();
             }
-            };
+            else {
+                if (this.getAttribute('class') == 'uncheck') {
+                    this.setAttribute('class', 'check');
+        
+                    save();
+                }
+                else {
+                    this.setAttribute('class', 'uncheck');
+         
+                    save();
+                }
+            }
+
         }
-        }
-        for (d of del) {
-            if(d.onclick == null){
-            d.onclick=function () {
-                
-                let n = del.indexOf(this);
-                ul.removeChild(lists[n]);
-                i = i-1;
-                lists.splice(n,1);
-                del.splice(n,1);
-                tick.splice(n,1);
-                para.splice(n,1);
-            };
-        }
-        }
+        save();
     }
     
+
 })
+function save(){
+    localStorage.setItem('uli', ul.innerHTML);
+ 
+}
+function get(){
+    console.log("Callign get");
+    ul.innerHTML = localStorage.getItem('uli');
+    let listItem = document.querySelectorAll('ul li');
+    console.log(listItem);
+    for(list of listItem){
+        list.onclick= function (event) {
+            console.dir(event);
+            if (event.target.nodeName == "SPAN") {
+      
+                ul.removeChild(this);
+                save();
+                
+            }
+            else {
+                if (this.getAttribute('class') == 'uncheck') {
+                    this.setAttribute('class', 'check');
+              
+                    save();
+                    
+                }
+                else {
+                    this.setAttribute('class', 'uncheck');
+                    
+                    save();
+                    
+                }
+            }
+
+        }
+    }
+
+}
